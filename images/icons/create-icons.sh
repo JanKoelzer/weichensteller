1cd5d672a683b27c2c8866243c1e556c7ced2e4d
jan <jth.koelzer@web.de>
2025-11-13 16:00:45 +0100

    Create icon for Google Play.

--- images/icons/create-icons.sh
+++ images/icons/create-icons.sh
@@ -6,7 +6,10 @@ done
 convert icon64.png icon64.ico
 
 # Android
-convert -size 432x432 xc:none \( icon264.png -geometry +10+0 \) -gravity center -composite android-foreground-icon.png
-convert ../background.png -gravity center -crop 432x432+0+0 +repage android-background-icon.png
+convert -size 432x432 xc:none \( icon264.png -geometry +10+0 \) -gravity center -composite android-foreground-icon432.png
+convert -size 512x512 xc:none \( icon432.png -geometry +10+0 \) -gravity center -composite android-foreground-icon512.png
+convert ../background.png -gravity center -crop 432x432+0+0 +repage android-background-icon432.png
+convert ../background.png -gravity center -crop 512x512+0+0 +repage android-background-icon512.png
+convert android-background-icon512.png android-foreground-icon512.png -gravity center -composite android-composed512.png
 
 
