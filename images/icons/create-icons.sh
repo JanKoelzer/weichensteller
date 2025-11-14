4f6a5215fe95fa47aa157904d9a71e26134d97f2
jan <jth.koelzer@web.de>
2025-11-14 18:18:26 +0100

    Reduce number of icon files.

--- images/icons/create-icons.sh
+++ images/icons/create-icons.sh
@@ -1,9 +1,9 @@
-for i in 64 128 192 264 256 432 512 1024; do
+for i in 16 32 48 64 128 192 256 264 432 512 1024; do
     inkscape icon.svg -o "icon${i}.png" -w $i
 done
 
 # ICO for Windows
-convert icon64.png icon64.ico
+convert icon16.png icon48.png icon32.png icon64.png icon128.png icon256.png -colors 256 icon-multisize.ico
 
 # Android
 convert -size 432x432 xc:none \( icon264.png -geometry +10+0 \) -gravity center -composite android-foreground-icon432.png
@@ -12,4 +12,7 @@ convert ../background.png -gravity center -crop 432x432+0+0 +repage android-back
 convert ../background.png -gravity center -crop 512x512+0+0 +repage android-background-icon512.png
 convert android-background-icon512.png android-foreground-icon512.png -gravity center -composite android-composed512.png
 
+for i in 16 32 48 64 128 256 264 512; do
+    rm "icon${i}.png"
+done
 
