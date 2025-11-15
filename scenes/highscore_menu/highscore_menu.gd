ff2cb4df5e7a6abaec2230617bcd2ba450dc02f3
jan <jth.koelzer@web.de>
2025-11-15 10:14:58 +0100

    Show highscore in a grid.

--- scenes/highscore_menu/highscore_menu.gd
+++ scenes/highscore_menu/highscore_menu.gd
@@ -3,9 +3,15 @@ extends VBoxContainer
 
 @onready
 var highscore_labels := [
-	%HighscoreTabContainer/Age0/HighscoreLabel,
-	%HighscoreTabContainer/Age1/HighscoreLabel,
-	%HighscoreTabContainer/Age2/HighscoreLabel,
+	%HighscoreTabContainer/Age0/HighscoreGrid/HighscoreLabel,
+	%HighscoreTabContainer/Age1/HighscoreGrid/HighscoreLabel,
+	%HighscoreTabContainer/Age2/HighscoreGrid/HighscoreLabel,
+]
+@onready
+var highscore_grids := [
+	%HighscoreTabContainer/Age0/HighscoreGrid,
+	%HighscoreTabContainer/Age1/HighscoreGrid,
+	%HighscoreTabContainer/Age2/HighscoreGrid,
 ]
 
 # Called when the node enters the scene tree for the first time.
@@ -17,13 +23,39 @@ func _ready() -> void:
 
 
 func _on_highscore_updated(highscore: Dictionary) -> void:
+	for l: Label in highscore_labels:
+		l.visible = false
+		
 	for group_idx: int in Highscore.AgeGroups.size():
 		var group := Highscore.AgeGroupNames[group_idx] as String
-		highscore_labels[group_idx].text = ""
+		create_sep_row(highscore_grids[group_idx])
 		
-		for i: int in highscore[group].size():
+		for i: int in highscore[group].size():			
 			var entry := highscore[group][i] as Dictionary
-			highscore_labels[group_idx].text += "%s. %s, %s, %s\n" % [i+1, entry.name, roundi(entry.score), entry.date]
+			var values := [str(i+1) + ".", entry.name.substr(0, 30), str(roundi(entry.score)), entry.date] as Array[String]
+			create_row(highscore_grids[group_idx], values)
+			create_sep_row(highscore_grids[group_idx])
+
+
+func create_row(grid_container: GridContainer, values: Array[String]) -> void:
+	grid_container.add_child(VSeparator.new())
+	for j in values.size():
+		var l := create_label(values[j], j == 1)
+		grid_container.add_child(l)
+		grid_container.add_child(VSeparator.new())
+
+
+func create_label(s: String, expand: bool = false) -> Label:
+	var l := Label.new()
+	l.text = s
+	if expand:
+		l.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	return l
+
+
+func create_sep_row(grid_container: GridContainer) -> void:
+	for j in range(grid_container.columns):
+		grid_container.add_child(HSeparator.new())
 
 
 func _on_close_button_pressed() -> void:
