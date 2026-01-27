b7d746be8e44ec484c11e482941833d8326e25a5
jan <jth.koelzer@web.de>
2026-01-27 20:54:10 +0100

    Use correct syntax for montage/bash.

--- images/odg/process-images.sh
+++ images/odg/process-images.sh
@@ -31,25 +31,25 @@ done
 
 
 cd "png"
-montage flags[1-2].png -tile 1x2 -background 'transparent' -geometry +0+0 flags.png
+montage flags{1..2}.png -tile 1x2 -background 'transparent' -geometry +0+0 flags.png
 cp "flags.png" "../../flags.png"
 echo "png/flags.png has been created and copied to ../"
 
 
 echo "Montage railsXXX.png…"
-montage rails[1-6].png -tile 6x1 -background 'transparent' -geometry +0+0 stations.png
+montage rails{1..6}.png -tile 6x1 -background 'transparent' -geometry +0+0 stations.png
 montage rails7.png rails8.png rails9.png rails10.png -tile 1x4 -background 'transparent' -geometry +0+0 trains1.png 
 montage rails11.png rails12.png rails13.png rails14.png -tile 1x4 -background 'transparent' -geometry +0+0 trains2.png 
 montage rails15.png rails16.png rails17.png rails18.png -tile 1x4 -background 'transparent' -geometry +0+0 trains3.png 
 montage rails19.png rails20.png rails21.png rails22.png -tile 1x4 -background 'transparent' -geometry +0+0 trains4.png 
 montage rails23.png rails24.png rails25.png rails26.png -tile 1x4 -background 'transparent' -geometry +0+0 trains5.png 
 montage rails27.png rails28.png rails29.png rails30.png -tile 1x4 -background 'transparent' -geometry +0+0 trains6.png 
-montage trains[1-6].png -tile 6x1 -background 'transparent' -geometry +0+0 trains.png 
+montage trains{1..6}.png -tile 6x1 -background 'transparent' -geometry +0+0 trains.png 
 
 montage stations.png trains.png -tile 1x2 -background 'transparent' -geometry +0+0 colored.png 
 
 echo "Montage tracksXXX.png…"
-montage tracks[1-6].png -tile 6x1 -background 'transparent' -geometry +0+0 tracks.png
+montage tracks{1..6}.png -tile 6x1 -background 'transparent' -geometry +0+0 tracks.png
 
 echo "Montage tracks, trains and stations…"
 montage tracks.png colored.png -tile 1x2 -background 'transparent' -geometry +0+0 rails.png
