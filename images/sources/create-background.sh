73fe6aabe6ec7fe18f750c36f1adf3ec1681f90d
jan <jth.koelzer@web.de>
2026-08-01 14:35:32 +0200

    New background image

---
+++ images/sources/create-background.sh
@@ -0,0 +1,1 @@
+convert background.jpg -gravity southwest -crop 5158x1190+0+0 -rotate 180 -colorspace gray -negate -level 8%,60% -blur 0x2.0 -fill "#80652c" -colorize 80% -brightness-contrast 25x-5 ../background.png
